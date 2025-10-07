51d03c304a3f761eff9fab0207d7c02e50c1e0a2
Pratyush Jaishanker <pjaishan@usc.edu>
2025-10-06 17:19:23 -0700

    working manager signup

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,11 +1,38 @@
 // FRONT-END STUBS ONLY — replace these with real Firebase/Firestore calls.
 // Each function should throw an Error on failure and return a simple object on success.
+import { auth, db } from './firebase';
+import {createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore"; 
 
-export async function registerProgram({ name, location, programUsername, managerEmail, password }) {
+export async function registerProgram({ name, city, state, departments = [], managerFirstName, managerLastName, managerEmail, password }) {
     // TODO: replace with real backend call
     await sleep(600);
+    createUserWithEmailAndPassword(auth, managerEmail, password)
+    .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+    });
     // Pretend the backend generates a unique programId (e.g., 'PW-ABC123')
     const programId = 'PW-' + Math.random().toString(36).slice(2, 8).toUpperCase();
+    //set up Firestore documents 
+    await setDoc(doc(db, "manager_info", auth.currentUser.uid), {
+        hospital_name: name,
+        hospital_city: city,
+        hospital_state: state,
+        departments: departments,
+        first_name: managerFirstName,
+        last_name: managerLastName,
+        email: managerEmail,
+        program_id: programId,
+        manager_id: auth.currentUser.uid
+    });
+
     return { programId };
 }
 
